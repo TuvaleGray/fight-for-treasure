071ce70d23abb9fdb758ce3289e53f1f5e161ac2
TuvaleGray <tuvale.gray@marcellin.school.nz>
2025-11-27 10:43:44 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -102,7 +102,7 @@ function random () {
             2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
             2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
             `, SpriteKind.projectilefromblueenemypleasework)
-        tiles.placeOnRandomTile(badlazer, assets.tile`transparency16`)
+        tiles.placeOnRandomTile(badlazer, sprites.dungeon.darkGroundCenter)
     }
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -124,8 +124,8 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `, person, -300, 10)
-    pause(500)
     music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
+    pause(200)
 })
 sprites.onOverlap(SpriteKind.a, SpriteKind.Player, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
@@ -140,6 +140,7 @@ sprites.onDestroyed(SpriteKind.enemyb, function (sprite) {
     info.changeScoreBy(1)
     game.splash("The final round", "")
     scene.cameraShake(8, 10000)
+    music.play(music.createSong(hex`0078000408020504001c00100500640000041e000004000000000000000000000000000a04000424000400080001290c00100001251400180001241c002000012924002800012a28002c00012405001c000f0a006400f4010a00000400000000000000000000000000000000021e0000000400011908000c0001191000140001192000240001192c003000012007001c00020a006400f401640000040000000000000000000000000000000003180000000400011e10001400011e18001c00011e28002c00011e08001c000e050046006603320000040a002d0000006400140001320002010002060030003400011d09010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c800240000000100010b08000900010b10001100010b18001900010b20002100010b28002900010b`), music.PlaybackMode.UntilDone)
     enemy1GAMEBEGINS = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . 8 8 8 . . . . . 
@@ -181,9 +182,8 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         ..........................
         ..........................
         `, person, 300, -10)
-    pause(500)
-    sprites.destroy(spear)
     music.play(music.melodyPlayable(music.zapped), music.PlaybackMode.UntilDone)
+    pause(200)
 })
 info.onScore(4, function () {
     chest2 = sprites.create(img`
@@ -633,6 +633,7 @@ let enemy_monkey: Sprite = null
 let person: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(person)
+tiles.setCurrentTilemap(tilemap`level1`)
 person = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
